dee44e1bcaa1092be098aa19c385666e8b395981
Vlad Tymchenko <vlad.timchenko@gmail.com>
2024-01-12 12:28:32 +0100

    first commit

---
+++ webServer.js
@@ -0,0 +1,15 @@
+'use strict';
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello Guys');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app;
